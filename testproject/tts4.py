9a377f2a1393cf29f703ba5e6c6612b4e4ac646c
boat83 <66733033+boat83@users.noreply.github.com>
2021-08-09 19:33:11 +0200

    Add files via upload

--- testproject/tts4.py
+++ testproject/tts4.py
@@ -10,16 +10,18 @@ URL = "https://black-moss-0a0440e03.azurestaticapps.net/tts4.html"
 driver.get(URL)
 
 submit = driver.find_element_by_id('submit')
-
+# 100 gombnyomas vegrehajtasa ciklussal
 for _ in range(100):
     submit.click()
 result = driver.find_elements_by_tag_name('li')
 counter = 0
+# vegigiteralunk is megszamoljuk a fej ertekeket
 for i in result:
     if i.text == 'fej':
         counter = counter + 1
 print(counter)
 
+# vizsgalat min 30 fej
 assert counter > 30
-print('Tobb mint harmic')
+print('Tobb mint harmic, helyes mukodes')
 driver.close()
