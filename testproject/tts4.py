8ea0c080d2f7e9151425bad4717de9ee20d799d7
boat83 <66733033+boat83@users.noreply.github.com>
2021-08-09 19:29:41 +0200

    Add files via upload

---
+++ testproject/tts4.py
@@ -0,0 +1,25 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+driver = webdriver.Chrome(ChromeDriverManager().install())
+URL = "https://black-moss-0a0440e03.azurestaticapps.net/tts4.html"
+
+driver.get(URL)
+
+submit = driver.find_element_by_id('submit')
+
+for _ in range(100):
+    submit.click()
+result = driver.find_elements_by_tag_name('li')
+counter = 0
+for i in result:
+    if i.text == 'fej':
+        counter = counter + 1
+print(counter)
+
+assert counter > 30
+print('Tobb mint harmic')
+driver.close()
